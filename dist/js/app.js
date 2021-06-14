110b937c0cc80ccba09c9fab4299bb661e065a8d
alexellevu <newnapster83@gmail.com>
2021-06-14 09:27:57 +0200

    applicato sass su css

---
+++ dist/js/app.js
@@ -0,0 +1,35 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/***/ (() => {
+
+alert('Hi Laravel mix');
+
+/***/ }),
+
+/***/ "./src/app.scss":
+/*!**********************!*\
+  !*** ./src/app.scss ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Undefined variable.\n  ╷\n2 │     background-color:$sfondo;\r\n  │                      ^^^^^^^\n  ╵\n  src\\app.scss 2:22  root stylesheet\n    at processResult (C:\\Users\\Alex\\Desktop\\CORSO BOOLEAN\\JAVASCRIPT\\html-sass-first\\node_modules\\webpack\\lib\\NormalModule.js:703:19)\n    at C:\\Users\\Alex\\Desktop\\CORSO BOOLEAN\\JAVASCRIPT\\html-sass-first\\node_modules\\webpack\\lib\\NormalModule.js:809:5\n    at C:\\Users\\Alex\\Desktop\\CORSO BOOLEAN\\JAVASCRIPT\\html-sass-first\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at C:\\Users\\Alex\\Desktop\\CORSO BOOLEAN\\JAVASCRIPT\\html-sass-first\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (C:\\Users\\Alex\\Desktop\\CORSO BOOLEAN\\JAVASCRIPT\\html-sass-first\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\Users\\Alex\\Desktop\\CORSO BOOLEAN\\JAVASCRIPT\\html-sass-first\\node_modules\\sass-loader\\dist\\index.js:54:7\n    at Function.call$2 (C:\\Users\\Alex\\Desktop\\CORSO BOOLEAN\\JAVASCRIPT\\html-sass-first\\node_modules\\sass\\sass.dart.js:93283:16)\n    at _render_closure1.call$2 (C:\\Users\\Alex\\Desktop\\CORSO BOOLEAN\\JAVASCRIPT\\html-sass-first\\node_modules\\sass\\sass.dart.js:81648:12)\n    at _RootZone.runBinary$3$3 (C:\\Users\\Alex\\Desktop\\CORSO BOOLEAN\\JAVASCRIPT\\html-sass-first\\node_modules\\sass\\sass.dart.js:27468:18)\n    at _FutureListener.handleError$1 (C:\\Users\\Alex\\Desktop\\CORSO BOOLEAN\\JAVASCRIPT\\html-sass-first\\node_modules\\sass\\sass.dart.js:26017:19)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	__webpack_modules__["./src/app.js"]();
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/app.scss"]();
+/******/ 	
+/******/ })()
+;
